44c48eea1b8795b8837de976438985fa8b5bda1c
Jim Drygiannakis <makingartstudios@gmail.com>
2018-11-18 10:47:47 +0200

    microprofiler

---
+++ include/jx/profiler.h
@@ -0,0 +1,13 @@
+#ifndef JX_PROFILER_H
+#define JX_PROFILER_H
+
+#include <jx/logger.h>
+#include <jx/sys.h>
+#include <bx/timer.h>
+
+#define JX_PROFILE_START(id) int64_t __profileStart_##id = bx::getHPCounter();
+#define JX_PROFILE_END(id, text) \
+    int64_t __profileDelta_##id = bx::getHPCounter() - __profileStart_##id; \
+    JX_LOG_DEBUG("%s took %f msec\n", text, 1000.0 * (double)__profileDelta_##id / (double)bx::getHPFrequency())
+
+#endif
