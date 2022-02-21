703e17f90ff337fa0d3d7d5d335ed3c868fcbd06
Jim Drygiannakis <makingartstudios@gmail.com>
2022-02-21 08:30:40 +0200

    utf8.cpp copyright and disclaimer

--- src/utf8.cpp
+++ src/utf8.cpp
@@ -1,3 +1,30 @@
+// Code is based on physfs_unicode.c https://github.com/SuperTux/physfs/blob/master/src/physfs_unicode.c
+// Adapted to match the coding style of the rest of the library.
+// This is not the original source file. Some functions are missing and a couple of others have been added.
+// I don't remember if I made any changes to the original source code except styling changes.
+//
+// Copyright(c) 2001 - 2018 Ryan C.Gordon and others.
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter itand redistribute it
+// freely, subject to the following restrictions :
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software.If you use this software in a
+// product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+// Ryan C.Gordon <icculus@icculus.org>
+//
 #include <jx/utf8.h>
 
 namespace jx
